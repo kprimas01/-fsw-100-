892526a83c9cac4807f80cb7e92d409cc75aad5c
kprimas01 <65737910+kprimas01@users.noreply.github.com>
2020-09-30 20:22:12 -0400

    week1

---
+++ week1/index.js
@@ -0,0 +1,6 @@
+
+    function f() {
+        // var t = document.getElementById("hi").value; commit out becouse of no value
+        document.getElementById("hi").innerHTML = "cupcake";
+    }
+f()
